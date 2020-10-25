fbcad864b917861a2c3d40cc4a4325e5fc6825c9
Eduardo Barrancos <eduardobarrancps11488@gmail.com>
2020-10-25 21:38:15 +0000

    Make Dir

--- runTests.sh
+++ runTests.sh
@@ -3,6 +3,12 @@
 #make directory of outputs with given name. if already exists does nothing
 #for cicle to run each file in inputs directory. for cicle to run 0 - max multiple threads for each file.
 
+if [ ! -d "$2" ]
+then
+    mkdir $2
+fi
+
+
 FILES=$(ls ${1})
 
 for filename in $FILES; do
