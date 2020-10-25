bd9945fade678a404a969f17dee56ee999c728fc
Rita Duarte <anaritard.2000@gmail.com>
2020-10-25 18:44:31 +0000

    New changes

---
+++ runTests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+make -f Makefile || exit
+
+
+#make directory of outputs with given name. if already exists does nothing
+
+if [ ! -d "$2" ]
+then
+    mkdir ./$2
+fi
+
+#for cicle to run each file in inputs directory. for cicle to run 0 - max multiple threads for each file.
+
+FILES=$(ls inputs/*.txt)
+
+for filename in $FILES; do
+    for ((i=0; i<=$3; i++)); do
+        echo "InputFile=$filename NumThreads=$i"
+        ./tecnicofs "$filename" "$filename-$i" "$i" "mutex"
+    done 
+done 
+
+mv 
+
+
