e4d84c6abffe5e9a8d774aba737d83bb5c328d0d
Rita Duarte <anaritard.2000@gmail.com>
2020-10-25 19:30:59 +0000

    Output files to output directory

--- runTests.sh
+++ runTests.sh
@@ -4,7 +4,7 @@
 
 if [ ! -d "$2" ]
 then
-    mkdir -m 777 $2
+    mkdir $2
 fi
 
 #for cicle to run each file in inputs directory. for cicle to run 0 - max multiple threads for each file.
@@ -15,5 +15,6 @@ for filename in $FILES; do
     for ((i=0; i<=$3; i++)); do
         echo "InputFile=$filename NumThreads=$i"
         ./tecnicofs "$filename" "$filename-$i.txt" "$i" "mutex"
+        mv "$filename-$i.txt" $2
     done
 done 
