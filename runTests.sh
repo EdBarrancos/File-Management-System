96372403e43c1a499a305b73285aac1785307f55
Rita Duarte <anaritard.2000@gmail.com>
2020-10-25 22:19:59 +0000

    Fixed minor syntax errors

--- runTests.sh
+++ runTests.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 #make directory of outputs with given name. if already exists does nothing
-#for cicle to run each file in inputs directory. for cicle to run 0 - max multiple threads for each file.
 
-if [ ! -d "$2" ]
+if [[ ! -d "$2" ]]
 then
     mkdir $2
 fi
 
+#for cicle to run each file in inputs directory. for cicle to run 0 - max multiple threads for each file.
+#prints the last line of variable
 
 FILES=$(ls ${1})
 
 for filename in $FILES; do
     for i in $(seq 0 ${3}); do
         echo "InputFile=${filename} NumThreads=$i"
-        file="${filename%.*}"
-        timeoutput=$(./tecnicofs "${1}/${filename}" "${2}/$file-${i}.txt" "${i}" "mutex")
-        echo "${timeoutput##*$'\n'}"
+        file=${filename%.*}
+        timeoutput=$(./tecnicofs ${1}/${filename} ${2}/$file-${i}.txt ${i} mutex)
+        echo ${timeoutput##*$'\n'}
     done
 done 
