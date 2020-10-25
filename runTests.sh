de5e2a4699bdd9680fa01dc849a0bb6284cad3af
Rita Duarte <anaritard.2000@gmail.com>
2020-10-25 20:32:50 +0000

    Fixed input tecnicofs

--- runTests.sh
+++ runTests.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 #make directory of outputs with given name. if already exists does nothing
-
-if [ ! -d "$2" ]
-then
-    mkdir $2
-fi
-
 #for cicle to run each file in inputs directory. for cicle to run 0 - max multiple threads for each file.
 
-FILES=$(ls inputs/*.txt)
+FILES=$(ls ${1})
 
 for filename in $FILES; do
-    for ((i=0; i<=$3; i++)); do
-        echo "InputFile=$filename NumThreads=$i"
-        ./tecnicofs "$filename" "$filename-$i.txt" "$i" "mutex"
-        mv "$filename-$i.txt" $2
+    for ((i=0; i<=${3}; i++)); do
+        echo "InputFile=${filename} NumThreads=$i"
+        timeoutput=$(./tecnicofs "${1}/${filename}" "${2}/${filename}-${i}.txt" "${i}" "mutex")
     done
 done 
