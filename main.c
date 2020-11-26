64877c10b8f56a66908835d0f4464cf908d212f0
Rita Duarte <anaritard.2000@gmail.com>
2020-11-26 16:16:37 +0000

    Fixed applycommands

--- main.c
+++ main.c
@@ -20,11 +20,14 @@
 #include "thr/threads.h"
 #include "er/error.h"
 
-//server constants
-//#define DIRSERVER "/tmp/"
+//server constants and variables
 #define INDIM 30
 #define OUTDIM 512
 char nameServer[108];
+int sockfd;
+struct sockaddr_un server_addr;
+socklen_t addrlen;
+char *path;
 
 int TRUE = 1;
 int numberThreads = 0;
@@ -130,7 +133,7 @@ void applyCommands(list* List){
     
     while(TRUE){
 
-        lockMutex();
+/*         lockMutex();
         
         if (!getFinishedState(Queue) || !emptyQueue(Queue)){
             unlockMutex();
@@ -138,30 +141,48 @@ void applyCommands(list* List){
 
             if (command == NULL){
                 continue;
-            }
+            }*/
 
             char token;
             char typeAndName[MAX_FILE_NAME];
-            char name[MAX_FILE_NAME];
-            int numTokens = sscanf(command, "%c %s %s", &token, name, typeAndName);
-            free(command);
+            char name[MAX_FILE_NAME]; 
+            struct sockaddr_un client_addr;
+            char in_buffer[INDIM], out_buffer[OUTDIM];
+            int c;
+
+            addrlen=sizeof(struct sockaddr_un);
+            c = recvfrom(sockfd, in_buffer, sizeof(in_buffer)-1, 0, (struct sockaddr *)&client_addr, &addrlen);
+
+            if (c <= 0) 
+                continue;
+            //Preventivo, caso o cliente nao tenha terminado a mensagem em '\0', 
+            in_buffer[c]='\0';
+            
+            printf("Recebeu mensagem de %s\n", client_addr.sun_path);
+
+            int numTokens = sscanf(in_buffer, "%c %s %s", &token, name, typeAndName);
+            //free(command);
             if (numTokens < 2)
                 errorParse("Error: invalid command in Queue\n");
 
             int searchResult;
 
+            c = sprintf(out_buffer, "%s", in_buffer);
+
             switch (token) {
                 case 'c':
                     switch (typeAndName[0]) {
                         case 'f':
-                            printf("Create file: %s\n", name);
+                            //printf("Create file: %s\n", name);
                             create(name, T_FILE, List);
-                            List = freeItemsList(List, unlockItem);
+                            List = freeItemsList(List, unlockItem);            
+                            sendto(sockfd, out_buffer, c+1, 0, (struct sockaddr *)&client_addr, addrlen);
                             break;
                         case 'd':
-                            printf("Create directory: %s\n", name);
+                            //printf("Create directory: %s\n", name);
                             create(name, T_DIRECTORY, List);
                             List = freeItemsList(List, unlockItem);
+                            sendto(sockfd, out_buffer, c+1, 0, (struct sockaddr *)&client_addr, addrlen);
                             break;
                         default:
                             errorParse("Error: invalid node type\n");
@@ -169,22 +190,25 @@ void applyCommands(list* List){
                     break;
                 case 'l': 
                     searchResult = lookup(name, List, 0);
-                    if (searchResult >= 0)
-                        printf("Search: %s found\n", name);
-                    else
-                        printf("Search: %s not found\n", name);
+                    //if (searchResult >= 0)
+                    //    printf("Search: %s found\n", name);
+                    //else
+                    //    printf("Search: %s not found\n", name);
                     List = freeItemsList(List, unlockItem);
+                    sendto(sockfd, out_buffer, c+1, 0, (struct sockaddr *)&client_addr, addrlen);
                     break;
                 case 'd':
-                    printf("Delete: %s\n", name);
+                    //printf("Delete: %s\n", name);
                     delete(name, List);
                     List = freeItemsList(List, unlockItem);
+                    sendto(sockfd, out_buffer, c+1, 0, (struct sockaddr *)&client_addr, addrlen);
                     break;
 
                 case 'm':
-                    printf("Move: %s to %s\n", name, typeAndName);
+                    //printf("Move: %s to %s\n", name, typeAndName);
                     move(name, typeAndName, List);
                     List = freeItemsList(List, unlockItem);
+                    sendto(sockfd, out_buffer, c+1, 0, (struct sockaddr *)&client_addr, addrlen);
                     break;
                     
                 default: { /* error */
@@ -234,12 +258,6 @@ void setInitialValues(char *argv[]){
 
 int main(int argc, char* argv[]) {
 
-    //server variables
-    int sockfd;
-    struct sockaddr_un server_addr;
-    socklen_t addrlen;
-    char *path;
-
     //time variables
     struct timeval tvinicio;
     struct timeval tvfinal;
@@ -279,7 +297,7 @@ int main(int argc, char* argv[]) {
     }
 
     
-    while (1) {
+/*     while (1) {
         struct sockaddr_un client_addr;
         char in_buffer[INDIM], out_buffer[OUTDIM];
         int c;
@@ -299,7 +317,7 @@ int main(int argc, char* argv[]) {
         sendto(sockfd, out_buffer, c+1, 0, (struct sockaddr *)&client_addr, addrlen);
 
     }
-
+ */
     /* init synch system */
     initLockMutex();
     
