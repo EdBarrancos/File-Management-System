3ae5f16fb131b5319c8b8ea6438b17d1f0df1cc0
Rita Duarte <anaritard.2000@gmail.com>
2020-11-30 19:17:23 +0000

    MNOR FIX

--- main.c
+++ main.c
@@ -58,11 +58,9 @@ void finishingModifyingCommand(){
 void startQuiescenteCommand(){
     lockMutex();
     quiescenteThreads++;
-    printf("Modifyin Threads: %d\n",modifyingThreads);
     while(modifyingThreads != 0)
         wait(&waitQuiescente);
 
-    printf("While Modifyin Threads: %d\n",modifyingThreads);
     unlockMutex();
 
 }
